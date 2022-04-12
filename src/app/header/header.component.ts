55a23dfef569f3fa362ae459e5f8f63d1837288e
heiligerhuegel <79452134+heiligerhuegel@users.noreply.github.com>
2022-04-12 18:40:34 +0200

    style: changed header and npm uninstalled bootstrap

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatChipsModule } from '@angular/material/chips';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
