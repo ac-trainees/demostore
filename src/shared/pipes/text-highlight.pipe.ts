006a2b78078b612817e11553e72976edace8d654
Natalija Sakuta <natalie.kutnaya@gmail.com>
2022-04-28 16:04:14 +0200

    test: create tests for a highlight pipe

--- src/shared/pipes/text-highlight.pipe.ts
+++ src/shared/pipes/text-highlight.pipe.ts
@@ -5,11 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TextHighlightPipe implements PipeTransform {
 
     transform(text: string, searchQuery: string): string {
+
+      if (!searchQuery) {
+        return text;
+      }
+
       var regex = new RegExp(searchQuery, 'gi'); //g - global match (find all matches rather than stopping after the first match) / i case-insensitive search
       const match = text.match(regex);
 
-      return searchQuery ?
-        text.replace(regex, `<span class="highlight">${match}</span>`) : text;
+      return text.replace(regex, `<span class="highlight">${match}</span>`);
     }
  }
 
